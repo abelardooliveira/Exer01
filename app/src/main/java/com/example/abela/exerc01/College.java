928da3f2a7ba4184c4b73e5b0c777aabc6de2614
Abelardo <abelardosamuel@hotmail.com>
2018-03-01 20:45:22 -0300

    Inicio Exercicios

---
+++ app/src/main/java/com/example/abela/exerc01/College.java
@@ -0,0 +1,26 @@
+package com.example.abela.exerc01;
+
+/**
+ * Created by abela on 01/03/2018.
+ */
+
+public class College {
+    private String name;
+    private String site;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+}
